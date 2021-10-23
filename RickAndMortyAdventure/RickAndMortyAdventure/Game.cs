6b303259f567dcafa9025dd4448a0a0bcf33fbf3
Philip Goforth <77207179+DrDiscoChickenShack@users.noreply.github.com>
2021-10-23 13:51:10 -0500

    menu and logo done

--- RickAndMortyAdventure/RickAndMortyAdventure/Game.cs
+++ RickAndMortyAdventure/RickAndMortyAdventure/Game.cs
@@ -9,17 +9,68 @@ namespace RickAndMortyAdventure
     {
         public void Start()
         {
-            
+
+
+
+            Title = "Rick and Morty Simulator.";
+            RunMainMenu();
 
             
-
-            string prompt = "Welcome to another Rick and Morty Adventure! What would you like to do?";
+        }
+        private void RunMainMenu()
+        {
+            string prompt = @"
+██████╗ ██╗ ██████╗██╗  ██╗     █████╗ ███╗   ██╗██████╗     ███╗   ███╗ ██████╗ ██████╗ ████████╗██╗   ██╗
+██╔══██╗██║██╔════╝██║ ██╔╝    ██╔══██╗████╗  ██║██╔══██╗    ████╗ ████║██╔═══██╗██╔══██╗╚══██╔══╝╚██╗ ██╔╝
+██████╔╝██║██║     █████╔╝     ███████║██╔██╗ ██║██║  ██║    ██╔████╔██║██║   ██║██████╔╝   ██║    ╚████╔╝ 
+██╔══██╗██║██║     ██╔═██╗     ██╔══██║██║╚██╗██║██║  ██║    ██║╚██╔╝██║██║   ██║██╔══██╗   ██║     ╚██╔╝  
+██║  ██║██║╚██████╗██║  ██╗    ██║  ██║██║ ╚████║██████╔╝    ██║ ╚═╝ ██║╚██████╔╝██║  ██║   ██║      ██║   
+╚═╝  ╚═╝╚═╝ ╚═════╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝     ╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝      ╚═╝                   
+          _______. __  .___  ___.  __    __   __          ___   .___________.  ______   .______      
+         /       ||  | |   \/   | |  |  |  | |  |        /   \  |           | /  __  \  |   _  \     
+        |   (----`|  | |  \  /  | |  |  |  | |  |       /  ^  \ `---|  |----`|  |  |  | |  |_)  |    
+         \   \    |  | |  |\/|  | |  |  |  | |  |      /  /_\  \    |  |     |  |  |  | |      /     
+     .----)   |   |  | |  |  |  | |  `--'  | |  `----./  _____  \   |  |     |  `--'  | |  |\  \----.
+     |_______/    |__| |__|  |__|  \______/  |_______/__/     \__\  |__|      \______/  | _| `._____|
+                                                                                                
+Welcome to another Rick and Morty Adventure! What would you like to do?
+(Use arrow keys and Enter to select.)";
             string[] options = { "Play", "About", "Exit" };
             Menu mainMenu = new Menu(prompt, options);
             int selectedIndex = mainMenu.Run();
-
-            WriteLine("Press any key to exit");
+            switch (selectedIndex)
+            {
+                case 0:
+                    RunChoice();
+                    break;
+                case 1:
+                    DisplayAbout();
+                    break;
+                case 2:
+                    ExitGame();
+                    break;
+            }
+        }
+        private void ExitGame()
+        {
+            WriteLine("\nPress any key to exit...");
             ReadKey(true);
+            Environment.Exit(0);
+        }
+        private void DisplayAbout()
+        {
+            Clear();
+            WriteLine("This simulator was created by Philip Goforth.");
+            WriteLine("Help was provided by Googling and YouTube tutorials...");
+            WriteLine("Assets from http://patorjk.com/software/taag");
+            WriteLine("Press any key to return to the menu.");
+            ReadKey(true);
+            RunMainMenu();
+        }
+        private void RunChoice()
+        {
+            WriteLine("game goes here");
+            ExitGame();
         }
     }
 }
