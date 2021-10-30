df00fd24b277fd74fe1e789601b7bc80eed5b27f
Philip Goforth <77207179+DrDiscoChickenShack@users.noreply.github.com>
2021-10-30 05:12:19 -0500

    added rick quips

--- RickAndMortyAdventure/RickAndMortyAdventure/Game.cs
+++ RickAndMortyAdventure/RickAndMortyAdventure/Game.cs
@@ -69,8 +69,153 @@ namespace RickAndMortyAdventure
         }
         private void RunChoice()
         {
-            WriteLine("game goes here");
-            ExitGame();
+            Clear();
+            WriteLine("You are asleep and suddenly your bedroom door flings open!!\n\nRick-'Hey little buddy...Hey how you doing in here??'\n\n");
+            WriteLine("Morty-'Oh geez Rick what are you doing in here??'\n\nRick-'I need you to come to the garage and help me fix my Ionic Difibulizer!'\n\n");
+            
+            ReadKey();
+            Clear();
+            WriteLine("You enter the garage and notice a strange looking device on Ricks workbench.\n\n");
+            WriteLine("Rick-'Now Morty what do you say can you hand me a screwdriver so I can finish my Ionic Difibulizer?'\n\nMorty-'Sure thing Rick here's a screwdriver..'");
+            WriteLine("\n\nRick takes the screw driver and starts tightening a screw on the machine...\n\n");
+            
+            ReadKey();
+            Clear();
+            WriteLine("Rick-'Alrighty, One screw turn...Two screw turns...'\n\nSuddenly the device explodes injuring rick and catching the house on fire!!\n\n ");
+            WriteLine("You've got to get out of here!! What do you do?!?!?!\n\n");
+            ReadKey();
+            Clear();
+            string decision = "Quick make a split decision!!";
+            string[] portals = { "'Oh geez Rick'", "Grab the portal gun and pull the trigger!", "Run away!" };
+            Menu portalMenu = new Menu(decision, portals);
+            int selectedIndex = portalMenu.Run();
+            switch (selectedIndex)
+            {
+                case 0:
+                    RickQuip();
+                    break;
+                case 1:
+                    Portal();
+                    break;
+                case 2:
+                    MortyDies();
+                    break;
+            }
+            Clear();
+            WriteLine("Rick- 'Aaaaand awaaaay we go!'\n\n\nPress any key...");
+            ReadKey();
+            Clear();
+            RunMainMenu();
+
+        }
+        public void MortyDies()
+        {
+            Random r = new Random();
+            int randomDeath = r.Next(0, 7);
+            switch (randomDeath)
+            {
+                case 1:
+                    WriteLine();
+                    break;
+                case 2:
+                    WriteLine();
+                    break;
+                case 3:
+                    WriteLine();
+                    break;
+                case 4:
+                    WriteLine();
+                    break;
+                case 5:
+                    WriteLine();
+                    break;
+                case 6:
+                    WriteLine();
+                    break;
+            }
+        }
+        public void RickQuip()
+        {
+            Randomizer();
+            ReadKey();
+            Clear();
+            string decision = "Quick choose run or portal!!";
+            string[] portals = { "Grab the portal gun and pull the trigger!", "Run away!" };
+            Menu portalMenu = new Menu(decision, portals);
+            int selectedIndex = portalMenu.Run();
+            switch (selectedIndex)
+            {
+                case 2:
+                    RickQuip();
+                    break;
+                case 0:
+                    Portal();
+                    break;
+                case 1:
+                    MortyDies();
+                    break;
+            }
+        }
+        public void Randomizer()
+        {
+            Random r = new Random();
+            int randomQuip = r.Next(0, 7);
+            switch (randomQuip)
+            {
+                case 1:
+                    Clear();
+                    WriteLine("\n\nRick-'Morty you piece of $#!7!!'");
+                    break;
+                case 2:
+                    Clear();
+                    WriteLine("\n\nRick-'Calm down Morty stop freaking out!!'");
+                    break;
+                case 3:
+                    Clear();
+                    WriteLine("\n\nRick-'You wanna die Morty?!? Do you!?!'");
+                    break;
+                case 4:
+                    Clear();
+                    WriteLine("\n\nRick-'Oh I'm sorry Morty are you having a hard time being stupid!!'");
+                    break;
+                case 5:
+                    Clear();
+                    WriteLine("\n\nRick-'There is no God in your face God!!'");
+                    break;
+                case 6:
+                    Clear();
+                    WriteLine("\n\nRick-'I don't want to die you idiot!'");
+                    break;
+            }
+        }
+        public void Portal()
+        {
+            Clear();
+            ForegroundColor = ConsoleColor.Green;
+            WriteLine("\n\n\t\t\t\t\t\tyou open a portal and jump in...");
+            Random r = new Random();
+            int portalSelect = r.Next(0, 100);
+            switch (portalSelect)
+            {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+            }
         }
     }
 }
