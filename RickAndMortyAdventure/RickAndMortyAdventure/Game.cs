1e8d12609e1931aa1b4d21957992a0a38ce1eef4
Philip Goforth <77207179+DrDiscoChickenShack@users.noreply.github.com>
2021-10-30 13:09:14 -0500

    added more portals

--- RickAndMortyAdventure/RickAndMortyAdventure/Game.cs
+++ RickAndMortyAdventure/RickAndMortyAdventure/Game.cs
@@ -99,7 +99,15 @@ namespace RickAndMortyAdventure
                     Portal();
                     break;
                 case 2:
+                    WriteLine("Rick-'Dont run away you coward!!\n\n");
+                    ReadKey();
+                    Clear();
+                    WriteLine("Rick pulls out a plasma pistol and shoots you to death...\n\n");
+                    ReadKey();
+                    Clear();
                     MortyDies();
+                    ReadKey();
+                    Clear();
                     break;
             }
             Clear();
@@ -109,32 +117,43 @@ namespace RickAndMortyAdventure
             RunMainMenu();
 
         }
-        public void MortyDies()
+        public static void MortyDies()
         {
             Random r = new Random();
-            int randomDeath = r.Next(0, 7);
-            switch (randomDeath)
+            int randomQuip = r.Next(0, 3);
+            switch (randomQuip)
             {
                 case 1:
-                    WriteLine();
+                    Clear();
+                    WriteLine("Rick- 'Morty! Morty wake up!'\n\n");
+                    ReadKey();
+                    Clear();
+                    WriteLine("Morty-'Wha...What! Where am I?!?!'\n\n");
+                    ReadKey();
+                    Clear();
+                    WriteLine("Rick-'Your room!'\n\n'You Were having a bad dream!!'\n\n");
+                    ReadKey();
+                    Clear();
+                    WriteLine("Rick-'Ready to go on another adventure?'\n\nRick pulls out the portal gun..");
+                    ReadKey();
+                    Clear();
                     break;
                 case 2:
-                    WriteLine();
+                    Clear();
+                    WriteLine("\n\nYou wake up in the garage with Rick standing over you.\n\n");
+                    ReadKey();
+                    Clear();
+                    WriteLine("\n\nRick-'Good you're awake. I need your help..'\n\n");
+                    ReadKey();
+                    Clear();
+                    WriteLine("\n\nRick pulls out the portal gun and pulls the trigger.\n\n");
+                    ReadKey();
+                    Clear();
                     break;
-                case 3:
-                    WriteLine();
-                    break;
-                case 4:
-                    WriteLine();
-                    break;
-                case 5:
-                    WriteLine();
-                    break;
-                case 6:
-                    WriteLine();
-                    break;
+
             }
         }
+
         public void RickQuip()
         {
             Randomizer();
@@ -153,7 +172,10 @@ namespace RickAndMortyAdventure
                     Portal();
                     break;
                 case 1:
+                    WriteLine("You got consumed by the flames and died...");
                     MortyDies();
+                    ReadKey();
+                    Clear();
                     break;
             }
         }
@@ -270,7 +292,13 @@ namespace RickAndMortyAdventure
         }
         static void BlenderDimension()
         {
+            WriteLine("You accidentally transported to the Blender Dimension and instantly got minced!!");
+            ReadKey();
+            Clear();
             WriteLine("You are dead from blenders");
+            ReadKey();
+            Clear();
+            MortyDies();
             ReadKey();
             Clear();
         }
@@ -279,16 +307,73 @@ namespace RickAndMortyAdventure
             WriteLine("You are at Blips and Chips");
             ReadKey();
             Clear();
+            string decision = "Would you like to stay at Blips and Chips or go somewhere else?";
+            string[] portals = { "Portal somewhere else..", "Stay and Play!" };
+            Menu portalMenu = new Menu(decision, portals);
+            int selectedIndex = portalMenu.Run();
+            switch (selectedIndex)
+            {
+                case 2:
+                    
+                    break;
+                case 0:
+                    
+                    PortalGun.PortalFluid();
+                    WriteLine($"Only {PortalGun.PortalFluid()} trips left with this amount of portal fluid...");
+                    OpenPortal();
+                    ReadKey();
+                    Clear();
+                    break;
+                case 1:
+                    BlipsAndChitzStay();
+                    break;
+            }
         }
         static void CornUniverse()
         {
             WriteLine("Everything is corn");
             ReadKey();
             Clear();
+            string decision = "Would you like to stay and stare at corn or go somewhere else?";
+            string[] portals = { "Portal somewhere else..", "Stay and...corn?" };
+            Menu portalMenu = new Menu(decision, portals);
+            int selectedIndex = portalMenu.Run();
+            switch (selectedIndex)
+            {
+                case 2:
+
+                    break;
+                case 0:
+                    
+                    PortalGun.PortalFluid();
+                    WriteLine($"Only {PortalGun.PortalFluid()} trips left with this amount of portal fluid...");
+                    OpenPortal();
+                    ReadKey();
+                    Clear();
+                    break;
+                case 1:
+                    CornStay();
+                    ReadKey();
+                    Clear();
+                    break;
+            }
         }
         static void MortysRoom()
         {
             WriteLine("You are in your room");
+            ReadKey();
+            Clear();
+            Clear();
+            WriteLine("Rick- 'Morty! Morty wake up!'\n\n");
+            ReadKey();
+            Clear();
+            WriteLine("Morty-'Wha...What! Where am I?!?!'\n\n");
+            ReadKey();
+            Clear();
+            WriteLine("Rick-'Your room!\t\tYou Were having a bad dream!!'\n\n");
+            ReadKey();
+            Clear();
+            WriteLine("Rick-'Ready to go on another adventure?'\n\nRick pulls out the portal gun..");
             ReadKey();
             Clear();
         }
@@ -297,10 +382,48 @@ namespace RickAndMortyAdventure
             WriteLine("The sun is screaming!");
             ReadKey();
             Clear();
+            string decision = "Would you like to stay and listen to the sun scream or go somewhere else?";
+            string[] portals = { "Portal somewhere else..", "Stay and get screamed to death." };
+            Menu portalMenu = new Menu(decision, portals);
+            int selectedIndex = portalMenu.Run();
+            switch (selectedIndex)
+            {
+                case 2:
+
+                    break;
+                case 0:
+                    
+                    PortalGun.PortalFluid();
+                    WriteLine($"Only {PortalGun.PortalFluid()} trips left with this amount of portal fluid...");
+                    OpenPortal();
+                    ReadKey();
+                    Clear();
+                    break;
+                case 1:
+                    ScreamStay();
+                    ReadKey();
+                    Clear();
+                    break;
+            }
         }
         static void ButtWorld()
         {
-            WriteLine("You are in butt world");
+            WriteLine("You look around and notice everything is butts.\n\nThe ground is butts...the clouds are butts...");
+            ReadKey();
+            Clear();
+            WriteLine("The ground is butts...\n\n");
+            ReadKey();
+            Clear();
+            WriteLine("the clouds are butts...");
+            ReadKey();
+            Clear();
+            WriteLine("You realize that the air is unbreathable and must be mostly methane from all the farts.");
+            ReadKey();
+            Clear();
+            WriteLine("Everything turns dark as you fade into a fart coma...");
+            ReadKey();
+            Clear();
+            MortyDies();
             ReadKey();
             Clear();
         }
@@ -309,10 +432,86 @@ namespace RickAndMortyAdventure
             WriteLine("You are at the citadel");
             ReadKey();
             Clear();
+            string decision = "Would you like to stay at the citadel or go somewhere else?";
+            string[] portals = { "Portal somewhere else..", "Stay and enjoy the Rick overlords." };
+            Menu portalMenu = new Menu(decision, portals);
+            int selectedIndex = portalMenu.Run();
+            switch (selectedIndex)
+            {
+                case 2:
+
+                    break;
+                case 0:
+                    
+                    PortalGun.PortalFluid();
+                    WriteLine($"Only {PortalGun.PortalFluid()} trips left with this amount of portal fluid...");
+                    OpenPortal();
+                    ReadKey();
+                    Clear();
+                    break;
+                case 1:
+                    CitadelStay();
+                    ReadKey();
+                    Clear();
+                    break;
+            }
         }
         static void HolographicSimulation()
         {
             WriteLine("You are in a simulation");
+            ReadKey();
+            Clear();
+            string decision = "Would you like to stay in the simulation forever or go somewhere else?";
+            string[] portals = { "Portal somewhere else..", "Stay and...simulate?" };
+            Menu portalMenu = new Menu(decision, portals);
+            int selectedIndex = portalMenu.Run();
+            switch (selectedIndex)
+            {
+                case 2:
+
+                    break;
+                case 0:
+                    
+                    PortalGun.PortalFluid();
+                    WriteLine($"Only {PortalGun.PortalFluid()} trips left with this amount of portal fluid...");
+                    OpenPortal();
+                    ReadKey();
+                    Clear();
+                    break;
+                case 1:
+                    SimulationStay();
+                    ReadKey();
+                    Clear();
+                    break;
+            }
+        }
+        public static void SimulationStay()
+        {
+            WriteLine("everything is a simulation");
+            ReadKey();
+            Clear();
+        }
+        public static void CitadelStay()
+        {
+            WriteLine("life at the citadel");
+            ReadKey();
+            Clear();
+        }
+        public static void ScreamStay()
+        {
+            WriteLine("Death from screaming");
+            ReadKey();
+            Clear();
+        }
+        public static void CornStay()
+        {
+            WriteLine("Corn");
+            ReadKey();
+            Clear();
+        }
+        public static void BlipsAndChitzStay()
+        {
+            WriteLine("You stay at Blips and Chipz forever");
             ReadKey();
             Clear();
         }
